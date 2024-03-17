7c3c7ba4817408a5d80247f35232e230fb8ca967
Aidan Ih <aidan.h.ih@gmail.com>
2024-03-16 23:20:04 -0400

    init

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import { HashRouter } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router";
+
+function App() {
+  return (
+    <div>
+      Hello this is my test URL presently transferring content over to it
+    </div>
+  );
+}
+
+export default App;
